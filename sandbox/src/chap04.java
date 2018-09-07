8fb92c1b15ac32a1685c76569f164ba8e2516457
striggle <stbigrigg@gmail.com>
2018-09-06 21:45:45 -0400

    thurs homework

---
+++ sandbox/src/chap04.java
@@ -0,0 +1,13 @@
+
+public class chap04 {
+
+	public static void main(String[] args) {
+		String numbers = "";
+		
+		for (int i=0; i<20; i+=2) {
+			numbers += i + " ";
+		}
+		System.out.println(numbers);
+	}
+
+}
